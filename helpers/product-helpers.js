488615bd2841d714919778c97c52f1822d948463
jagath gopan <jagathgopan304@gmail.com>
2020-11-25 17:04:10 +0530

    first

---
+++ helpers/product-helpers.js
@@ -0,0 +1,61 @@
+var db=require('../config/connection')
+var collection=require('../config/collections')
+var promise=require('promise');
+const { resolve } = require('promise');
+/*  const {reject} = require('promise'); 
+ const {}= require('../routes/user'); 
+  */
+var objectId=require('mongodb').ObjectId
+module.exports={
+    
+addProduct:(product, callback)=>{
+   
+    db.get().collection('product').insertOne(product).then((data)=>{
+     
+        callback(data.ops[0]._id)
+        product.Price=parseInt(product.Price)
+    })  
+},
+getAllProducts:()=>{
+    return new promise (async (resolve,reject)=>{
+        let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+        resolve(products)
+    })
+},
+deleteProduct:(prodId)=>{
+    return new promise ((resolve,reject)=>{
+        console.log(prodId);
+        console.log(objectId(prodId));
+        db.get().collection(collection.PRODUCT_COLLECTION).removeOne({_id:objectId(prodId)}).then((response)=>{
+            console.log(response);
+            resolve(response)   
+        })
+    })
+},
+getProductDetails:(prodId)=>{
+    return new promise ((resolve,reject)=>{
+        db.get().collection(collection.PRODUCT_COLLECTION).findOne({_id:objectId(prodId)}).then((product)=>{
+        resolve(product )
+        console.log(product);
+        })
+    })
+},
+updateProduct:(prodId,prodDetails)=>{
+    return new promise((resolve,reject)=>{
+        db.get().collection(collection.PRODUCT_COLLECTION)
+        .updateOne({_id:objectId(prodId)},{
+            $set:{
+                Name:prodDetails.Name,
+                Category:prodDetails.Category,
+                Description:prodDetails.Description,
+                Price:prodDetails.Price
+            }
+         
+        }).then((response)=>{
+            resolve()
+        })
+    })
+}
+}
+
+
---
+++ routes/user.js
@@ -0,0 +1,98 @@
+
+const { response } = require('express');
+var express = require('express');
+var router = express.Router();
+const  productHelpers = require('../helpers/product-helpers');
+const userHelpers=require('../helpers/user-helpers')
+
+const verifyLogin=(req,res,next)=>{
+  if(req.session.loggedIn){
+      next()
+  }else{
+    res.redirect('/login')
+  }
+}
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  let user=req.session.user
+  console.log(user)
+  let cartCount=null
+  if(req.session.user){
+  cartCount=await userHelpers.getCartCount(req.session.user._id)
+}
+    productHelpers.getAllProducts().then((products)=>{
+  
+      res.render('user/view-products', { admin:false,products,user,cartCount})
+    })
+
+});
+router.get('/login',(req,res)=>{
+  if(req.session.loggedIn){
+    res.redirect('/')
+  }else{
+
+    res.render('user/login',{'loginErr': req.session.loginErr});
+    req.session.loginErr=false
+  }
+ 
+})
+  router.get('/signup',(req,res)=>{
+    res.render('user/signup');
+})
+router.post('/signup',(req,res)=>{
+userHelpers.doSignup(req.body).then((response)=>{
+  console.log(response)
+  req.session.loggedIn=true
+  req.session.user=response
+  res.redirect('/')
+})
+ })
+ router.post('/login',(req,res)=>{
+   userHelpers.doLogin(req.body).then((response)=>{
+   if(response.status){
+    req.session.loggedIn=true
+    req.session.user=response.user
+    res.redirect('/')
+   }else{
+     req.session.loginErr="Invalid username or password"
+     res.redirect('login')
+   }
+   })
+     
+ }) 
+
+ router.get('/logout',(req,res)=>{
+   req.session.destroy()
+   res.redirect('/')
+ })
+ router.get('/cart',verifyLogin,async(req,res)=>{
+  let products=await userHelpers.getCartProducts(req.session.user._id)
+   
+ res.render('user/cart',{products,user:req.session.user})
+})
+
+router.get('/add-to-cart/:id',(req,res)=>{
+  console.log('API CALLED');
+    userHelpers.addToCart(req.params.id,req.session.user._id).then(()=>{
+    res.json({status:true})
+  })
+  
+})
+router.post('/change-product-quantity',(req,res)=>{
+  console.log(req.body);
+  userHelpers.changeProductQuantity(req.body).then((response)=>{    // here req.body contains the data object defined in ajax
+    res.json(response)
+    
+  })
+})
+router.post('/remove-cart-item',(req,res)=>{
+  userHelpers.removeCartItem(req.body).then((response)=>{
+    console.log('This is response');
+    res.json(response)    
+  })
+})
+router.get('/place-order',verifyLogin,async(req,res)=>{
+let total=await userHelpers.getTotalAmount(req.session.user._id)
+res.render('user/placeorder',{total})
+})
+module.exports = router;
